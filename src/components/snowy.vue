66757f1146426dde895b530ee954f08811e86f52
xiaob <xiaob@sucsoft.com>
2019-03-29 17:52:50 +0800

    add calendar

---
+++ src/components/snowy.vue
@@ -0,0 +1,108 @@
+<template>
+    <div id="snowy">
+        <div class="viewbox">
+            <i class="iconfont iconxuehua " v-for="item in snowList" :style="styleful(item)"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "snowy",
+        data() {
+            return {
+                snowList:[
+
+                ],
+                fn1:null,
+                fn2:null,
+                iii:0,
+                tiemr:0,
+            }
+        },
+        watch:{
+            snowList(){
+                if(this.iii>111){
+                    this.cancelSnow();
+                    setTimeout(()=>{
+                        this.snowy();
+                    },3000);
+                }
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            snowy(){
+                if(this.fn2){}
+                else{
+                    this.fn2 = setInterval(()=>{
+                        clearInterval(this.fn1);
+                        this.tiemr = Math.random()*300 + 200;
+                        this.fn1 = setInterval(()=>{
+                            let px = parseInt(Math.random() * 500);
+                            this.snowList.push({
+                                left:px,
+                                top:-40,
+                                fontSize:24 + Math.random()*12,
+                                rotate:0,
+                                opacity:1,
+                                transition: 3 + Math.random() * 2,
+                            });
+                            setTimeout(()=>{
+                                let top = document.body.clientHeight + parseInt(Math.random() * 500);
+                                let con1 = Math.random()>0.5 ? -1 : 1;
+                                let con2 = Math.random()>0.5 ? -1 : 1;
+                                this.$set(this.snowList,this.iii,{
+                                    left:this.snowList[this.iii].left + con2 * (Math.random()*200 + 100),
+                                    top:top,
+                                    fontSize:10 + Math.random()*8,
+                                    rotate: con1 * (Math.random() * 720 + 720),
+                                    opacity:0.2,
+                                });
+                                this.iii++;
+                            },50)
+                        },this.tiemr);
+                    },1000);
+                }
+
+
+            },
+            styleful(item){
+                let str = `left:${item.left}px;top:${item.top}px;opacity:${item.opacity};font-size:${item.fontSize}px;transform: rotate(${item.rotate}deg); transition:all ${item.transition}s linear;`;
+                return str;
+            },
+            cancelSnow(){
+                clearInterval(this.fn2);
+                clearInterval(this.fn1);
+                this.fn2 = null;
+                this.fn1 = null;
+                setTimeout(()=>{
+                    this.iii = 0;
+                    this.snowList = [];
+                },3000);
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    #snowy {
+        pointer-events: none;
+        .viewbox{
+            position: relative;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+        .iconxuehua{
+            font-size: 36px;
+            position: absolute;
+            left: 2px;
+            top: -40px;
+            color: #B7DFF4;
+            transition:all 5s linear;
+        }
+    }
+</style>
--- src/pages/trainingManagement/paperMng/paperDetail.vue
+++ src/pages/trainingManagement/paperMng/paperDetail.vue
@@ -39,7 +39,7 @@
             <myBtn type="cancel" text="取消" :handleClick="cancelSnow"></myBtn>
             <p style="position: absolute;bottom:30px">Vue中的render函数，适用于相同组件不同状态的css样式呈现。点击“下雪”，开启动画</p>
             <p style="position: absolute;bottom:10px">Vue中为了支持JSX，需要通过一个JSX的Babel的插件。</p>
-            <i class="iconfont iconxuehua " v-for="item in snowList" :style="styleful(item)"></i>
+            <snowy ref="snowy" style="width: 100%;height: 100%;position: absolute;left: 0;top: 0px;z-index: 1;"></snowy>
             <!--
             package.json文件配置
             devDependencies: {
@@ -78,13 +78,15 @@
     import randomBall from 'components/randomBall'
     import myBtn from 'components/myBtn'
     import calendar from 'components/calendar'
+    import snowy from 'components/snowy'
     export default {
         name: "paperDetail",
         components: {
             randomBall,
             mixSelect,
             myBtn,
-            calendar
+            calendar,
+            snowy
         },
         data() {
             return {
@@ -92,32 +94,14 @@
                 //squareHeight:document.body.clientHeight - 100 + `px`,
                 actIndex:3,
                 text:`xxxxx`,
-                snowList:[
-
-                ],
-                fn1:null,
-                fn2:null,
-                iii:0,
-                tiemr:0,
                 txt:``,
-
 
             }
         },
         mounted(){
             //window.vue = this;
+        },
 
-        },
-        watch:{
-            snowList(){
-                if(this.iii>111){
-                    this.cancelSnow();
-                    setTimeout(()=>{
-                        this.snowy();
-                    },3000);
-                }
-            }
-        },
         methods: {
             goit(){
                 this.$refs.random.startit();
@@ -132,55 +116,14 @@
                 this.$refs.random.setBall = val;
                 this.actIndex = index
             },
-            cancelSnow(){
-                clearInterval(this.fn2);
-                clearInterval(this.fn1);
-                this.fn2 = null;
-                this.fn1 = null;
-                setTimeout(()=>{
-                    this.iii = 0;
-                    this.snowList = [];
-                },3000);
-            },
             snowy(){
-                if(this.fn2){}
-                else{
-                    this.fn2 = setInterval(()=>{
-                        clearInterval(this.fn1);
-                        this.tiemr = Math.random()*300 + 200;
-                        this.fn1 = setInterval(()=>{
-                            let px = parseInt(Math.random() * 500);
-                            this.snowList.push({
-                                left:px,
-                                top:-40,
-                                fontSize:24 + Math.random()*12,
-                                rotate:0,
-                                opacity:1,
-                                transition: 3 + Math.random() * 2,
-                            });
-                            setTimeout(()=>{
-                                let top = document.body.clientHeight + parseInt(Math.random() * 500);
-                                let con1 = Math.random()>0.5 ? -1 : 1;
-                                let con2 = Math.random()>0.5 ? -1 : 1;
-                                this.$set(this.snowList,this.iii,{
-                                    left:this.snowList[this.iii].left + con2 * (Math.random()*200 + 100),
-                                    top:top,
-                                    fontSize:10 + Math.random()*8,
-                                    rotate: con1 * (Math.random() * 720 + 720),
-                                    opacity:0.2,
-                                });
-                                this.iii++;
-                            },50)
-                        },this.tiemr);
-                    },1000);
-                }
+                this.$refs.snowy.snowy();
+            },
+            cancelSnow(){
+                this.$refs.snowy.cancelSnow();
+            }
 
 
-            },
-            styleful(item){
-                let str = `left:${item.left}px;top:${item.top}px;opacity:${item.opacity};font-size:${item.fontSize}px;transform: rotate(${item.rotate}deg); transition:all ${item.transition}s linear;`;
-                return str;
-            },
 
 
         },
@@ -240,13 +183,6 @@
             }
         }
     }
-    .iconxuehua{
-        font-size: 36px;
-        position: absolute;
-        left: 2px;
-        top: -40px;
-        color: #B7DFF4;
-        transition:all 5s linear;
-    }
+
 
 </style>
